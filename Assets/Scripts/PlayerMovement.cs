b822d6e94a0b1b0d65be851e06471e9e73cfe348
kio <kianaxh@gmail.com>
2024-09-03 17:53:32 +1200

    Colllision triggers and camera movement
    integrated onCollision triggers for collecting orion's stars and camera movement so that camera moves with player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 10f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 8f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
